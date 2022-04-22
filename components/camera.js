6ba102ff98f09be2e4e9aa9740b30a14c037ede8
Apiwat Nongwa <>
2022-04-22 13:55:13 +0700

    :sparkles: capture photo

--- components/camera.js
+++ components/camera.js
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Webcam from 'react-webcam'
 import { useState, useRef, useCallback } from 'react'
 
@@ -6,22 +5,30 @@ const LiveCamera = () => {
   const webcamRef = useRef(null)
   const [imgSrc, setImgSrc] = useState(null)
   const [msg, setMsg] = useState('')
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: 'user',
+  }
 
   const capture = useCallback(() => {
-    if (webcamRef.current !== null) {
-      const imgSrc = webcamRef.current.getScreenshot()
-      setImgSrc(imgSrc)
-    } else {
-      setMsg('กรุณาเปิดกล้อง')
-    }
-  }, [webcamRef, setImgSrc])
+    const imageSrc = webcamRef.current.getScreenshot()
+    setImgSrc(imageSrc)
+  }, [webcamRef])
 
   return (
     <>
       {msg && <h2>{msg}</h2>}
-      <Webcam />
+      <Webcam
+        audio={false}
+        height={350}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={350}
+        videoConstraints={videoConstraints}
+      />
       <button onClick={capture}>Capture photo</button>
-      {imgSrc && <Image src={imgSrc} alt="Capture Image" />}
+      {imgSrc && <img src={imgSrc} alt="Capture Image" />}
     </>
   )
 }
